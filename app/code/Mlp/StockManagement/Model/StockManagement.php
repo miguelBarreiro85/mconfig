8a11d44d24b0bb23b224e7db701c44d6e0f79370
miguelbarreiro85 <miguelbarreiro85@gmail.com>
2021-02-01 21:57:26 +0000

    stockmanagement

--- app/code/Mlp/StockManagement/Model/StockManagement.php
+++ app/code/Mlp/StockManagement/Model/StockManagement.php
@@ -1,14 +1,30 @@
 <?php 
 namespace Mlp\StockManagement\Model;
+use Magento\Framework\Model\AbstractModel;
  
  
-class StockManagement {
+class StockManagement extends AbstractModel{
 
+	/**
+     * @var \Magento\Framework\Stdlib\DateTime
+     */
+	protected $_dateTime;
+	
+	/**
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init(\Mlp\StockManagement\Model\ResourceModel\StockManagement::class);
+    }
 	/**
 	 * {@inheritdoc}
 	 */
-	public function addProducts($param,$quantity)
+	public function addProducts($sku,$quantity)
 	{
-		return 'api GET return the sku ' . $param . 'and quantity' . $quantity;
+		$this->setData("sku",$sku);
+		$this->setData("quantity",$quantity);
+		$this->getResource()->save($this);
+		return 'api GET return the sku ' . $sku . 'and quantity' . $quantity;
 	}
 }
